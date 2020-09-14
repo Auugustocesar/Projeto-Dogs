079b9c943c49a3103bf88b1f3d1efcca8513dcdd
Augusto Cesar <cgu.augusto@gmail.com>
2020-09-14 01:34:53 -0300

    infinite scroll

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -4,13 +4,15 @@ import UserHeader from "./UserHeader";
 import Feed from "../Feed/Feed";
 import UserPhotoPost from "./UserPhotoPost";
 import UserStats from "./UserStats";
+import { UserContenxt } from "../../UserContext";
 
 const User = () => {
+  const { data } = React.useContext(UserContenxt)
   return (
     <section className="container">
       <UserHeader />
       <Routes>
-        <Route path="/" element={<Feed />} />
+        <Route path="/" element={<Feed user={data.id} />} />
         <Route path="postar" element={<UserPhotoPost />} />
         <Route path="estatisticas" element={<UserStats />} />
       </Routes>
